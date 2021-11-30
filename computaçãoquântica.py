b867dfaba4d0e2c50f815a8f40c7055e072b452e
walterjrbr <waugustojr@gmail.com>
2021-11-30 09:40:00 -0300

    Update computaçãoquântica.py

--- computaçãoquântica.py
+++ computaçãoquântica.py
@@ -1,36 +1,31 @@
 # -*- coding: utf-8 -*-
-"""ComputaçãoQuântica.ipynb"""
+"""ComputaçãoQuântica.ipynb """
 
 
 !pip install qiskit
 
-pip install qiskit[visualization]
+!pip install qiskit[visualization]
 
-
-
-!pip install pylatexenc
-
-!pip install qiskit --upgrade
-
-
+"""# Simulação dos estados quânticos de Hilbert"""
 
 from qiskit import *
 from qiskit.visualization import plot_histogram
 
-num_qubits=2
-num_bits=2
-qc=QuantumCircuit(num_qubits,num_bits)
+#num_qubits=3
+#num_bits=3
+#qc=QuantumCircuit(num_qubits,num_bits) # sintaxe
 
-qc
-
-qc.x(0)
+# Cria um circuito  com 3 qubits e 3 bits clássico 
+qc = QuantumCircuit(3,3)
+# qc.x([0,2])  # Faz porta X sobre os qubits 0,2
+# Faz porta H sobre os qubits 0,1 e 2
 qc.h(1)
-qc.measure(0,0)
-qc.measure(1,1)
+qc.h(2)
+qc.h(0)
+qc.measure([0,1,2], [0,1,2])
+qc.measure([1], [1])
 
-
-
-qc.draw(output='text')
+display(qc.draw())   # Mostra o circuito quântico
 
 backend = Aer.get_backend('qasm_simulator')
 
